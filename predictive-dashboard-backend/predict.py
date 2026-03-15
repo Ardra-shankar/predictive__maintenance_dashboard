afbd883686fe2517fb2a570acc2c3e016f2e5741
ardra <jaardra007@gmail.com>
2026-03-15 23:05:43 +0530

    git add .

--- predictive-dashboard-backend/predict.py
+++ predictive-dashboard-backend/predict.py
@@ -15,6 +15,7 @@ feature_names = [
 'op1','op2','op3',
 's1','s2','s3','s4','s5','s6','s7','s8','s9','s10',
 's11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21'
+
 ]
 
 df = pd.DataFrame([data], columns=feature_names)
